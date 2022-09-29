07d7ee1bbdd338934dd6158f1529c219ded2d7ba
Yassine Mejri <yassinemejri5@gmail.com>
2022-09-29 20:44:50 +0100

    mobile adaptations

--- src/app/section-two/section-two.component.ts
+++ src/app/section-two/section-two.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-section-two',
   templateUrl: './section-two.component.html',
-  styles: [
-  ]
+  styleUrls: ['./section-two.component.css']
 })
 export class SectionTwoComponent implements OnInit {
 
